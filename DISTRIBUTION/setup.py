bf2b3eb18f86eb59765153ae6a4fe26a279bae0d
Hans Terlouw <terlouw@astro.rug.nl>
2009-11-03 13:11:44 +0100

    Added lut directory and changed setup.py; added MANIFEST.in

--- DISTRIBUTION/setup.py
+++ DISTRIBUTION/setup.py
@@ -1,21 +1,21 @@
 from distutils.core import setup, Extension
 from distutils.sysconfig import get_python_inc, get_python_lib
 from kapteyn import __version__ as version
-import os, numpy
+import sys, os, numpy
 
 include_dirs = []
 numdir = os.path.dirname(numpy.__file__)
 ipath = os.path.join(numdir, numpy.get_include())
 include_dirs.append(ipath)
 
-description = """The Kapteyn Package is a collection of Python modules and applications
-made by the computer group of the Kapteyn Astronomical Institute,
-University of Groningen, The Netherlands.  The purpose of the package is
-to provide tools for the development of astronomical applications with
-Python.  This package is still being developed and possibly some
-interfaces can change in a next release.  Base module wcs has
-intensively been tested and seems to be stable.  The wcs module
-supports:
+description = """The Kapteyn Package is a collection of Python modules and
+applications developed by the computer group of the Kapteyn Astronomical
+Institute, University of Groningen, The Netherlands.
+The purpose of the package is to provide tools for the development of
+astronomical applications with Python.  This package is still being
+developed and possibly some interfaces can change in a next release. 
+Base module wcs has intensively been tested and should be stable.  The
+wcs module supports:
 
     * spatial and spectral coordinates
     * spectral translations (e.g. translate frequencies into velocities)
@@ -47,5 +47,7 @@ setup(
          include_dirs=include_dirs
       )
    ],
-   packages=['kapteyn']
+   package_dir={'kapteyn': 'kapteyn'},
+   packages=['kapteyn'],
+   package_data={'kapteyn': ['lut/*.lut']},
 )
