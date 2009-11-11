930b207609d43adb9a11609cbf4f2ddae2be7f2f
Hans Terlouw <terlouw@astro.rug.nl>
2009-11-11 13:14:30 +0100

    mu_xvruler changed

--- SPHINX/EXAMPLES/mu_xvruler.py
+++ SPHINX/EXAMPLES/mu_xvruler.py
@@ -11,16 +11,18 @@ mplim = f.Annotatedimage(frame)
 
 # Velocity - Dec
 grat = mplim.Graticule()
+
 xmax = grat.pxlim[1]+0.5; ymax = grat.pylim[1]+0.5
 ruler = grat.Ruler(xmax,0.5, xmax, ymax, lambda0 = 0.5, step=5.0/60.0, 
                    fun=lambda x: x*60.0, fmt=r"$%4.0f^\prime$", 
                    fliplabelside=True)
-ruler.setp_line(lw='2', color='r')
+ruler.setp_line(lw=2, color='r')
 ruler.setp_labels(clip_on=True, color='r')
+
 ruler2 = grat.Ruler(0.5,0.5, xmax, ymax, lambda0 = 0.5, step=5.0/60.0, 
                     fun=lambda x: x*60.0, fmt=r"$%4.0f^\prime$", 
                     fliplabelside=True)
-ruler2.setp_line(lw='2', color='b')
+ruler2.setp_line(lw=2, color='b')
 ruler2.setp_labels(clip_on=True, color='b')
 grat.setp_plotaxis("right", label="Offset (Arcsec)", visible=True)
 
