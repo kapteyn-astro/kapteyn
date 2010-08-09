4e5ddedcab174223d055bf13dc4d73f501b63b36
Hans Terlouw <terlouw@astro.rug.nl>
2010-08-09 14:46:06 +0200

    README-wcslib.txt and wcsconfig.h added.

---
+++ DISTRIBUTION/src/README-wcslib.txt
@@ -0,0 +1,5 @@
+The unpacked and unmodified WCSLIB distribution is placed in this directory.
+The Kapteyn Package's setup.py script uses its sources when building the
+kapteyn.wcs module. When a new version of WCSLIB is released, the old one
+can be removed and replaced by the new one. Then the variable 'wcslib_dir'
+in the setup.py script may have to be changed.
---
+++ DISTRIBUTION/src/wcsconfig.h
@@ -0,0 +1,18 @@
+/* wcsconfig.h.  Generated from wcsconfig.h.in by configure.  */
+/*============================================================================
+*
+*   wcsconfig.h is generated from wcsconfig.h.in by 'configure'.  It contains
+*   C preprocessor macro definitions for compiling WCSLIB 4.5.
+*
+*   Author: Mark Calabretta, Australia Telescope National Facility
+*   http://www.atnf.csiro.au/~mcalabre/index.html
+*   $Id: wcsconfig.h.in,v 4.5 2010/07/16 07:01:26 cal103 Exp $
+*===========================================================================*/
+
+/* WCSLIB library version number. */
+#define WCSLIB_VERSION 4.5
+
+/* Define to 1 if sincos() is available. */
+
+/* 64-bit integer data type. */
+#define WCSLIB_INT64 long long int
