6e821be56545b4fe2867bf418cfd35f8c79495db
Hans Terlouw <terlouw@astro.rug.nl>
2010-11-04 14:20:19 +0100

    Beta version increment

--- DISTRIBUTION/kapteyn/__init__.py
+++ DISTRIBUTION/kapteyn/__init__.py
@@ -10,4 +10,4 @@ __all__=['celestial', 'wcs', 'wcsgrat', 'tabarray', 'maputils',
          'mplutil', 'positions', 'shapes', 'rulers', 'filters',
          'interpolation']
 
-__version__='2.0.3b9'
+__version__='2.0.3b10'
