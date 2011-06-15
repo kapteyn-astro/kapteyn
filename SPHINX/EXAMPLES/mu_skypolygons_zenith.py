2d8ff2640a7d6d77d759130f325aff61f925ad84
Hans Terlouw <terlouw@astro.rug.nl>
2011-06-15 10:35:40 +0200

    updated example script: mu_skypolygons_zenith.py

--- SPHINX/EXAMPLES/mu_skypolygons_zenith.py
+++ SPHINX/EXAMPLES/mu_skypolygons_zenith.py
@@ -2,6 +2,20 @@ from kapteyn import maputils
 from matplotlib import pyplot as plt
 import numpy
 
+# This script shows that you can plot shapes that cross the pole.
+# A shape is plotted with respect to its center and the border points
+# are derived in a way that distance and angle are correct for a sphere.
+# This makes it impossible to have objects centered at the pole because at 
+# the pole, longitudes are undefined. To avoid this problem, one can shift
+# the center of such shapes a little as we have done with pcra and 
+# pcdec below.
+# The try excepts in this program is to catch problems with special 
+# projections (e.g. NCP where dec > 0)
+
+delta = 0.0001 
+pcra = delta
+pcdec = 90. -delta
+
 def shapes(proj, fig, plnr, crval2=0.0, **pv):
    naxis1 = 800; naxis2 = 800
    header = {'NAXIS': 2,  
@@ -30,33 +44,38 @@ def shapes(proj, fig, plnr, crval2=0.0, **pv):
    try:
       annim.Skypolygon("ellipse", cpos="5h00m 20d0m", major=50, minor=30,
                         pa=-30.0, fill=False)
+      print "Plotted ellipse with cpos='5h00m 20d0m', major=50, minor=30, pa=-30.0, fill=False"
    except:
       print "Failed to plot ellipse"
    # Ellipse at given pixel coordinates
    try:
       cpos = "%f %f"%(naxis1/2.0+20, naxis2/2.0+10)
-      annim.Skypolygon("ellipse", cpos=cpos, major=20, minor=10,
-                        pa=-30.0, fc='m')
+      annim.Skypolygon("ellipse", cpos=cpos, major=40, minor=10,
+                        pa=0.0, fc='m')
+      print "Plotted ellipse major=40, minor=10, pa=-30.0, fc='m'"
    except:
       print "Failed to plot ellipse"
    # Circle with radius in arc minutes
    try:
-      annim.Skypolygon("ellipse", cpos="0 deg 60 deg",
+      annim.Skypolygon("ellipse", xc=pcra, yc = pcdec, #cpos="0 deg 60 deg",
                      major=30, minor=30,
                      fc='g', alpha=0.3, lw=3, ec='r') 
+      print "Plotted red circle, green with red border transparent"
    except:
       print "Failed to plot circle"
    # Rectangle at the projection center
    try:
-      annim.Skypolygon("rectangle", cpos="pc pc", major=50, minor=20,
+      annim.Skypolygon("rectangle", xc=pcra, yc=pcdec, major=50, minor=20,
                      pa=30.0, ec='g', fc='b', alpha=0.3)
+      print "Plotted blue rectangle at projection center"
    except:
-      print "Failed to plot rectangle"
+      print "Failed to plot blue rectangle at projection center"
    # Square centered at 315 deg -45 deg and with size equal
    # to distance on sphere between 300,-30 and 330,-30 deg (=25.9)
    try:
       annim.Skypolygon("rectangle", cpos="315 deg -45 deg", major=25.9, minor=25.9,
                         pa=0.0, ec='g', fc='#ff33dd', alpha=0.8)
+      print "Plotted square with color #ff33dd"
    except:
       print "Failed to plot square"
    # Regular polygon with 6 angles at some position in galactic coordinates
@@ -64,6 +83,7 @@ def shapes(proj, fig, plnr, crval2=0.0, **pv):
       annim.Skypolygon("npoly", cpos="ga 102d11m35.239s ga 59d50m25.734", 
                         major=20, nangles=6,
                         ec='g', fc='y', alpha=0.3)
+      print "Plotted npoly in yellow"
    except:
       print "Failed to plot regular polygon"
    # Regular polygon as a triangle
@@ -71,6 +91,7 @@ def shapes(proj, fig, plnr, crval2=0.0, **pv):
       annim.Skypolygon("npolygon", cpos="ga 0 ga 90", 
                         major=70, nangles=3,
                         ec='g', fc='c', alpha=0.7)
+      print "Plotted npoly triangle in cyan"
    except:
       print "Failed to plot triangle"
    # Set of (absolute) coordinates, no prescription
@@ -78,6 +99,7 @@ def shapes(proj, fig, plnr, crval2=0.0, **pv):
    lats = [-30, -30, 0, 0]
    try:
       annim.Skypolygon(prescription=None, lons=lons, lats=lats, fc='r', alpha=0.9)
+      print "Plotted polygon without prescription"
    except:
       print "Failed to plot set of coordinates as polygon"
 
