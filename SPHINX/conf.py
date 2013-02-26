e9b87ae0f56f4f3a1852ea28c2cd487b5a89e28e
Hans Terlouw <terlouw@astro.rug.nl>
2013-02-26 22:39:31 +0100

    ignone kapteyn.bib (generated), copyright -> 2013

--- SPHINX/conf.py
+++ SPHINX/conf.py
@@ -52,7 +52,7 @@ master_doc = 'contents'
 
 # General information about the project.
 project = u'Kapteyn Package'
-copyright = u'2010-2012, Kapteyn Astronomical Institute'
+copyright = u'2010-2013, Kapteyn Astronomical Institute'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
